ee83705c0d072faa5531baba959778700f7bfa35
ProgrammerManan <mananmaheshwari9548@gmail.com>
2023-11-07 14:51:10 -0600

    host change

--- environment/main.py
+++ environment/main.py
@@ -64,5 +64,8 @@ def logout():
     session.clear()
     return redirect(url_for('hac_login'))
 
+# if __name__ == '__main__':
+#     app.run(debug=True, port=9999)
+
 if __name__ == '__main__':
-    app.run(debug=True, port=9999)
+    app.run(debug=False, host='0.0.0.0')
